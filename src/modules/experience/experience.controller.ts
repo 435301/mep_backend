115ccdaca34f0af820243c876a6d5cf8cea45fca
RICH LABZ <padmavathi.richlabz@gmail.com>
2026-07-27 15:42:21 +0530

    languages and experience get apis

--- src/modules/experience/experience.controller.ts
+++ src/modules/experience/experience.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ExperienceService } from './experience.service';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ROUTES } from 'src/common/constants/routes.constant';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/gaurds/roles.guard';
 
-@Controller('experience')
+@Controller(`${ROUTES.ADMIN}/experience`)
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class ExperienceController {
   constructor(
     private readonly experienceService: ExperienceService,
--- src/modules/languages/entities/language.controller.ts
+++ src/modules/languages/entities/language.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { LanguageService } from './language.service';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ROUTES } from 'src/common/constants/routes.constant';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/gaurds/roles.guard';
 
-@Controller('language')
+
+@Controller(`${ROUTES.ADMIN}/language`)
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class LanguageController {
   constructor(
     private readonly languageService: LanguageService,
